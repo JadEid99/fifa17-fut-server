5cf8b6f289c558f9475b99f098a2993c614b3800
Jad Eid <jad.n.eid@gmail.com>
2026-04-13 18:39:13 +0200

    Frida v6: dump LoginSM structure + fix type2=NULL by writing valid login type

--- frida_force_login.js
+++ frida_force_login.js
@@ -1,111 +1,90 @@
 /*
- * Frida v5: After PreAuth, directly send Login RPC on the same connection.
- * Bypasses the broken login state machine entirely.
+ * Frida v6: Fix type2=NULL in LoginStateMachine, then trigger Login.
  */
 var base = Process.getModuleByName('FIFA17.exe').base;
 function addr(off) { return base.add(off); }
-console.log('=== FIFA 17 Force Login v5 === Base=' + base);
+console.log('=== FIFA 17 Force Login v6 === Base=' + base);
+
+// Block all disconnects
+Interceptor.replace(addr(0x6db3e40), new NativeCallback(function(p1) {
+    console.log('[DISCONNECT] BLOCKED');
+}, 'void', ['pointer']));
 
 var preAuthCount = 0;
 
-// Hook PreAuth completion - after it fires, send Login on the same connection
 Interceptor.attach(addr(0x6e19a00), {
     onEnter: function(args) {
         preAuthCount++;
         var connMgr = args[0];
-        var result = args[1];
-        console.log('[PreAuth #' + preAuthCount + '] connMgr=' + connMgr + ' result=' + result);
-        
-        // Only act on the Q-triggered connection (2nd PreAuth)
-        if (preAuthCount < 2) {
-            console.log('[PreAuth] Skipping first auto-connect, waiting for Q press...');
-            return;
-        }
-        
-        console.log('[PreAuth] >>> Attempting to send Login RPC! <<<');
-        
-        // The connMgr object has the connection at +0x8 (inner connection manager)
-        // FUN_146e18170 sends an RPC: (connMgrInner+0xc08, callback_struct, this, callbackFn, ...)
-        // But this is too complex to call directly.
-        
-        // Simpler approach: find the Blaze connection's send function and send raw Login bytes
-        // The connection object is at connMgr+0x10 (the embedded login type object)
-        // Actually, let's try calling the login type's "execute" function directly
+        console.log('[PreAuth #' + preAuthCount + '] connMgr=' + connMgr);
         
         try {
-            // Read the BlazeHub from the handler
-            // connMgr is the ConnectionManager. BlazeHub is at connMgr - some offset.
-            // Actually, connMgr+0x8 = inner connection (the one that has the RPC system)
-            var innerConn = connMgr.add(0x8).readPointer();
-            console.log('[Login] innerConn=' + innerConn);
+            // Get the BlazeHub
+            var blazeHub = connMgr.add(0x8).readPointer();
+            // ConnMgr is at BlazeHub+0x750
+            var connMgrFromHub = blazeHub.add(0x750).readPointer();
+            console.log('[Fix] BlazeHub=' + blazeHub + ' ConnMgr(from hub)=' + connMgrFromHub);
             
-            // The inner connection has the RPC dispatch at +0xc08
-            // And the connection socket at +0xc50
-            var rpcDispatch = innerConn.add(0xc08);
-            var connSocket = innerConn.add(0xc50).readPointer();
-            console.log('[Login] rpcDispatch=' + rpcDispatch + ' connSocket=' + connSocket);
+            // LoginSM is at ConnMgr+0x1db0
+            var loginSM = connMgrFromHub.add(0x1db0);
+            console.log('[Fix] LoginSM=' + loginSM);
             
-            // Read the connection state
-            var connState = innerConn.add(0xb28).readU32();
-            console.log('[Login] connState=' + connState);
-            
-            // Check if connection is still open
-            if (connSocket.isNull()) {
-                console.log('[Login] Connection socket is NULL - already disconnected');
-                return;
+            // Dump LoginSM structure
+            var raw = loginSM.readByteArray(80);
+            var bytes = Array.from(new Uint8Array(raw));
+            console.log('[Fix] LoginSM raw (80 bytes):');
+            for (var i = 0; i < 80; i += 8) {
+                var val = loginSM.add(i).readPointer();
+                console.log('[Fix]   +0x' + i.toString(16) + ' = ' + val);
             }
             
-            // Try to find the ProtoSSL send function
-            // FUN_14612e960 is the ProtoSSL send function
-            // It takes: (connObj, data, length)
-            // But we need to send a properly formatted Blaze packet through the RPC layer
+            // Check type2 at +0x10
+            var type2 = loginSM.add(0x10).readPointer();
+            console.log('[Fix] type2=' + type2);
             
-            // Let's try a different approach: hook the disconnect function and PREVENT it
-            // Then the connection stays open and we can observe what happens
-            console.log('[Login] Preventing disconnect by replacing FUN_146db3e40...');
-            
+            if (type2.isNull()) {
+                console.log('[Fix] >>> type2 is NULL! Looking for SilentLogin type object... <<<');
+                
+                // The SilentLogin type should be at loginSM + 0x510 (offset 0xa2*8)
+                // Let's check if there's a valid object there
+                var silentLoginType = loginSM.add(0x510);
+                var slVtable = silentLoginType.readPointer();
+                console.log('[Fix] SilentLogin candidate at ' + silentLoginType + ' vtable=' + slVtable);
+                
+                // Also check loginSM + 0x560 (offset 0xac*8) for ExpressLogin
+                var expressLoginType = loginSM.add(0x560);
+                var elVtable = expressLoginType.readPointer();
+                console.log('[Fix] ExpressLogin candidate at ' + expressLoginType + ' vtable=' + elVtable);
+                
+                // Check loginSM + 0x38 (offset 0x07*8) for Login type 0
+                var loginType0 = loginSM.add(0x38);
+                var lt0Vtable = loginType0.readPointer();
+                console.log('[Fix] LoginType0 candidate at ' + loginType0 + ' vtable=' + lt0Vtable);
+                
+                // If any of these have valid vtables (in code section), use it
+                if (slVtable.compare(addr(0)) > 0 && slVtable.compare(addr(0x10000000)) < 0) {
+                    console.log('[Fix] >>> Writing SilentLogin type into type2! <<<');
+                    loginSM.add(0x10).writePointer(silentLoginType);
+                    console.log('[Fix] type2 is now: ' + loginSM.add(0x10).readPointer());
+                } else if (lt0Vtable.compare(addr(0)) > 0 && lt0Vtable.compare(addr(0x10000000)) < 0) {
+                    console.log('[Fix] >>> Writing LoginType0 into type2! <<<');
+                    loginSM.add(0x10).writePointer(loginType0);
+                    console.log('[Fix] type2 is now: ' + loginSM.add(0x10).readPointer());
+                } else {
+                    console.log('[Fix] No valid login type found to write');
+                }
+            } else {
+                console.log('[Fix] type2 is already set: ' + type2);
+            }
         } catch(e) {
-            console.log('[Login] Error: ' + e.message);
+            console.log('[Fix] Error: ' + e.message);
         }
     }
 });
 
-// REPLACE the disconnect function to prevent disconnection after PreAuth
-var disconnectCount = 0;
-Interceptor.replace(addr(0x6db3e40), new NativeCallback(function(param1) {
-    disconnectCount++;
-    console.log('[DISCONNECT #' + disconnectCount + '] BLOCKED! param1=' + param1);
-    // Don't call the original - just return
-    // This keeps the connection open after PreAuth
-}, 'void', ['pointer']));
-console.log('[HOOK] Disconnect function REPLACED (all disconnects blocked)');
+Interceptor.attach(addr(0x6e18170), { onEnter: function(a) { console.log('[send_RPC] >>> CALLED!'); } });
+Interceptor.attach(addr(0x6f2a270), { onEnter: function(a) { console.log('[LOGIN_SENDER] >>> CALLED!'); } });
+Interceptor.attach(addr(0x6e1e460), { onEnter: function(a) { console.log('[post_PreAuth] >>> CALLED!'); } });
+Interceptor.attach(addr(0x6e1c3f0), { onEnter: function(a) { console.log('[PreAuth_proc] >>> CALLED!'); } });
 
-// Hook send_RPC to see if anything tries to send
-Interceptor.attach(addr(0x6e18170), {
-    onEnter: function(args) {
-        console.log('[send_RPC] >>> cm=' + args[0] + ' cb=' + args[2] + ' fn=' + args[3]);
-    }
-});
-
-// Hook LOGIN_SENDER
-Interceptor.attach(addr(0x6f2a270), {
-    onEnter: function(args) {
-        console.log('[LOGIN] >>> SENDER CALLED! <<<');
-    }
-});
-
-// Hook post_PreAuth
-Interceptor.attach(addr(0x6e1e460), {
-    onEnter: function(args) {
-        console.log('[post_PreAuth] >>> CALLED! <<<');
-    }
-});
-
-// Hook PreAuth_processor
-Interceptor.attach(addr(0x6e1c3f0), {
-    onEnter: function(args) {
-        console.log('[PreAuth_proc] >>> CALLED! <<<');
-    }
-});
-
-console.log('=== Ready. Trigger connection. ===');
+console.log('=== Ready. ===');
