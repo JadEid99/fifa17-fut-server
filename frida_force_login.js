f1750c97c6e856bebbf03dabda394c80472d4485
Jad Eid <jad.n.eid@gmail.com>
2026-04-13 19:15:58 +0200

    Frida v9: fix type2 + let natural flow complete (no disconnect block)

--- frida_force_login.js
+++ frida_force_login.js
@@ -1,46 +1,55 @@
 /*
- * Frida v8: Force param_3=0 in response handler to trigger PreAuth processing.
- * This makes the game process the PreAuth response properly and set up the Login callback.
+ * Frida v9: Don't block disconnects. Just observe the full flow.
+ * v7 showed send_RPC was called (Login attempt). Let it complete naturally.
+ * Fix type2=NULL so the Login path works.
  */
 var base = Process.getModuleByName('FIFA17.exe').base;
 function addr(off) { return base.add(off); }
-console.log('=== FIFA 17 v8 === Base=' + base);
+console.log('=== FIFA 17 v9 === Base=' + base);
 
-// Block disconnects
-Interceptor.replace(addr(0x6db3e40), new NativeCallback(function(p1) {
-    console.log('[DISCONNECT] BLOCKED');
-}, 'void', ['pointer']));
-
-// THE FIX: Replace response handler to force param_3=0
-var originalHandler = new NativeFunction(addr(0x6e1cf10), 'void', ['pointer', 'pointer', 'int']);
-Interceptor.replace(addr(0x6e1cf10), new NativeCallback(function(param1, param2, param3) {
-    console.log('[RESPONSE_HANDLER] param1=' + param1 + ' param2=' + param2 + ' param3=0x' + param3.toString(16));
-    
-    if (param3 !== 0) {
-        console.log('[RESPONSE_HANDLER] >>> FORCING param3=0 to trigger PreAuth processing! <<<');
-        originalHandler(param1, param2, 0);
-    } else {
-        originalHandler(param1, param2, param3);
+// Fix type2=NULL in the response handler
+Interceptor.attach(addr(0x6e1cf10), {
+    onEnter: function(args) {
+        var p1 = args[0];
+        var p3 = args[2].toInt32();
+        console.log('[RESP] p1=' + p1 + ' p3=0x' + (p3 >>> 0).toString(16));
+        
+        if (p3 !== 0) {
+            // Login path - fix type2 if NULL
+            try {
+                var type2 = p1.add(0x10).readPointer();
+                console.log('[RESP] type2=' + type2);
+                if (type2.isNull()) {
+                    // Write LoginType0 (at +0x38) into type2
+                    var lt0 = p1.add(0x38);
+                    var lt0vt = lt0.readPointer();
+                    console.log('[RESP] Fixing type2: writing LoginType0 at ' + lt0 + ' (vtable=' + lt0vt + ')');
+                    p1.add(0x10).writePointer(lt0);
+                    console.log('[RESP] type2 fixed!');
+                }
+            } catch(e) { console.log('[RESP] Error: ' + e.message); }
+        }
     }
-    
-    console.log('[RESPONSE_HANDLER] returned');
-}, 'void', ['pointer', 'pointer', 'int']));
+});
 
-// Monitor what happens after
+// Monitor everything
 Interceptor.attach(addr(0x6e19a00), {
-    onEnter: function(args) { console.log('[PreAuth_complete] result=' + args[1]); }
+    onEnter: function(a) { console.log('[PreAuth_done] result=' + a[1]); }
 });
 Interceptor.attach(addr(0x6e18170), {
-    onEnter: function(args) { console.log('[send_RPC] >>> fn=' + args[3]); }
+    onEnter: function(a) { console.log('[send_RPC] >>> fn=' + a[3]); }
+});
+Interceptor.attach(addr(0x6db3e40), {
+    onEnter: function(a) { console.log('[DISCONNECT] p1=' + a[0]); }
 });
 Interceptor.attach(addr(0x6e1e460), {
-    onEnter: function(args) { console.log('[post_PreAuth] >>> CALLED!'); }
+    onEnter: function(a) { console.log('[post_PreAuth] >>> CALLED!'); }
 });
 Interceptor.attach(addr(0x6e1c3f0), {
-    onEnter: function(args) { console.log('[PreAuth_proc] >>> CALLED!'); }
+    onEnter: function(a) { console.log('[PreAuth_proc] >>> CALLED!'); }
 });
 Interceptor.attach(addr(0x6f2a270), {
-    onEnter: function(args) { console.log('[LOGIN_SENDER] >>> CALLED!'); }
+    onEnter: function(a) { console.log('[LOGIN_SENDER] >>> CALLED!'); }
 });
 
 console.log('=== Ready. ===');
